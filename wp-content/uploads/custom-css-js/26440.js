be59bcdf60c69d0ab30fb431b613a8985a8d4db6
matteo-belletti-kaffeina <94168560+matteo-belletti-kaffeina@users.noreply.github.com>
2023-05-19 11:11:28 +0200

    Larc Old Website pt1

---
+++ wp-content/uploads/custom-css-js/26440.js
@@ -0,0 +1,5 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+ $(".sc_promo").parent().removeClass('sc_layouts_hide_on_mobile');
+ </script>
+<!-- end Simple Custom CSS and JS -->
